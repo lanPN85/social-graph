41bcc5576222d5d4e261b4298ee6fbba49f3bc7d
= <phan.ngoclan58@gmail.com>
2018-12-28 12:56:14 +0700

    Better output format

--- conv_cluster.py
+++ conv_cluster.py
@@ -23,7 +23,7 @@ def parse_arguments():
     parser = ArgumentParser()
 
     parser.add_argument('-i', '--input', default='data/twitter/twitter-combined.txt')
-    parser.add_argument('-o', '--outdir', default='out/twitter')
+    parser.add_argument('-o', '--outdir', default=None)
     parser.add_argument('-p', '--partitions', type=int, default=8)
     parser.add_argument('-co', '--converter', default='naive', help=str(list(CONVERTERS.keys())))
     parser.add_argument('-cl', '--clusterer', default='laplacian', help=str(list(CLUSTERERS.keys())))
@@ -34,6 +34,11 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
+    if args.outdir is None:
+        inp_name = os.path.split(args.input)[-1].split('.')[:-1]
+        inp_name = '.'.join(inp_name)
+        args.outdir = os.path.join('out/', inp_name)
+
     os.makedirs(args.outdir, exist_ok=True)
     logzero.logfile('logs/conv_cluster.log', mode='w')
 
@@ -50,10 +55,24 @@ if __name__ == "__main__":
     del ug
 
     for i, gx in enumerate(comp_g):
+        outdir = os.path.join(args.outdir, 'component-%d' % i)
+        os.makedirs(outdir, exist_ok=True)
+
         logger.info('Component %d/%d' % (i+1, len(comp_g)))
         logger.info('Clustering')
         clu = CLUSTERERS[args.clusterer]
         clusters = clu(gx, partitions=args.partitions)
+        
+        out_name = 'clusters.txt'
+        out_path = os.path.join(outdir, out_name)
+        logger.info('Saving clusters to %s' % out_path)
+        save_clusters(clusters, out_path)
+
+        out_name = 'graph.txt'
+        out_path = os.path.join(outdir, out_name)
+        logger.info('Saving graph edges to %s' % out_path)
+        save_edges(g, out_path)
+        
         node_attrs = {
             'shape': 'point'
         }
@@ -64,22 +83,11 @@ if __name__ == "__main__":
             'arrowsize': '0.2',
             'arrowhead': 'open'
         }
-        
-        out_name = 'clusters-%d.txt' % i
-        out_path = os.path.join(args.outdir, out_name)
-        logger.info('Saving clusters to %s' % out_path)
-        save_clusters(clusters, out_path)
-
-        out_name = 'graph-%d.txt'
-        out_path = os.path.join(args.outdir, out_name)
-        logger.info('Saving graph edges to %s' % out_path)
-        save_edges(g, out_path)
-
         logger.info('Rendering %d clusters' % len(clusters))
-        visual.render_clusters(g, clusters, outdir=args.outdir,
-            name='clusters-%d' % i, no_label=True, node_attrs=node_attrs,
+        visual.render_clusters(g, clusters, outdir=outdir,
+            name='clusters', no_label=True, node_attrs=node_attrs,
             edge_attrs=edge_attrs, engine=args.engine)
         logger.info('Rendering original graph')
-        visual.render_graph(g, outdir=args.outdir, edge_attrs=edge_attrs,
-            name='graph-%d' % i, no_label=True, node_attrs=node_attrs,
+        visual.render_graph(g, outdir=outdir, edge_attrs=edge_attrs,
+            name='graph', no_label=True, node_attrs=node_attrs,
             engine=args.engine)
